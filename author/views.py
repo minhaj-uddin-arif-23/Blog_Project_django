992c84d015439c5b97304b21e8977ceb60b0dc83
Arif_1 <uarif1933@gmail.com>
2024-07-29 18:29:57 +0600

    Add author form

--- author/views.py
+++ author/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+def add_author(request):
+    author_form = forms.AuthorForm()
+    return render(request,'./author/add_author.html',{'form':author_form}) 
