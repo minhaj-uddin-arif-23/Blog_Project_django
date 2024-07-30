aea7ee2db83dc28bfa667fae264d98935f532c18
Arif_1 <uarif1933@gmail.com>
2024-07-30 08:57:13 +0600

    creat read update delete operation done

--- BLOG_PROJECT/views.py
+++ BLOG_PROJECT/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
-
+from Post.models import post
 def home(request):
-    return render(request,'home.html')
+    data = post.objects.all()
+    return render(request,'home.html',{'data':data})
--- Post/views.py
+++ Post/views.py
@@ -1,6 +1,8 @@
-
 from django.shortcuts import render,redirect
 from . import forms
+from . import models
+
+
 
 def add_post(request):
     if request.method == "POST":
@@ -11,3 +13,21 @@ def add_post(request):
     else:
         post_form = forms.PostForm()
     return render(request,'add_post.html',{'form':post_form}) 
+
+
+
+def edit_post(request,id):
+    Posts = models.post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=Posts)
+    if request.method == "POST":
+        post_form = forms.PostForm(request.POST,instance=Posts) # no cng but no problem instance=Post
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('home')
+    
+    return render(request,'add_post.html',{'form':post_form}) 
+
+def delete_post(request,id):
+    post = models.post.objects.get(pk=id)
+    post.delete()
+    return redirect('home')
